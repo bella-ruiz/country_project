ad2ac3fcbd9b8d5e0f61d4d4e6ca4eccb0402d59
bella-ruiz <bellabruiz@gmail.com>
2022-04-25 23:50:18 -0400

    Added links

--- freeze.py
+++ freeze.py
@@ -2,6 +2,12 @@
 
 from flask_frozen import Freezer
 
+from data import country
+
+@freezer.register_generator
+def country():
+    for item in country:
+        yield { 'id': item['id'] }
 # instead of "filename," below, use the name of the file that
 # runs YOUR Flask app - omit .py from the filename
 from country import app
@@ -11,4 +17,4 @@ app.config['FREEZER_RELATIVE_URLS'] = True
 freezer = Freezer(app)
 
 if __name__ == '__main__':
-    freezer.freeze()
+    freezer.freeze();
